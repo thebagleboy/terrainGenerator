5f489c9c9e6366ed098c63b17e512291182bfee0
thebagleboy <alfred.burgess95@gmail.com>
2019-08-28 15:24:30 +0800

    chunck work

--- chuckGenerator.cpp
+++ chuckGenerator.cpp
@@ -4,20 +4,27 @@
 const int _width = 32;
 const int _height = 32;
 
-int genNoise( int width, int height, int lacinarity, int percistance ){
-	float heights;
-	for(int w = 0; w < _width; w++)
+float genPerlon( float x, float y ){ return (x / y); }
+
+float genNoise( int width, int height, float scale, int lacinarity, int percistance )
+{
+	float heightmap[width][height];
+	if( scale >= 0 ){ scale = 0.001; }
+	for(int x = 0; x < width; x++)
 	{
-		for( int h = 0; h < _height; h++)
+		for( int y = 0; y < height; y++)
 		{
-			std::cout << rand() % _maxHeight;
+			float sampleX = x / scale, sampleY = y / scale;
+			float perlon = genPerlon(sampleX, sampleY);
+			heightmap[x][y] = perlon;
 		}
-		std::cout << "\n";
 	}
-	return 0;
+	// return heightmap;
+	std::cout << heightmap;
+	return (float)1;
 }
 
 int main()
 {
-	genNoice( 8, 8, 1, 1);
+	std::cout << genNoise( 8, 8, 0.5,  1, 1);
 }
